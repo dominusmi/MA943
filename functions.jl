47fe55c0454f97db58f00811812ea57e1ad4de34
Unknown <barp.edoardo@gmail.com>
2017-11-09 23:29:58 +0000

    Setting up nicely
    Having issues with BigFloat, less accurate than Float64

--- functions.jl
+++ functions.jl
@@ -1,10 +1,13 @@
-recursion64(a_n0::Float64, a_n1::Float64) = Float64(2*a_n0-8/9*a_n1);
-recursion32(a_n0::Float32, a_n1::Float32) = Float32(2*a_n0-8/9*a_n1);
-recursionBig(a_n0::BigFloat, a_n1::BigFloat) = BigFloat(2*a_n0-8/9*a_n1);
 
+# Functions for all recursion types
+recursion64(a_n0::Float64, a_n1::Float64) = Float64(2*a_n0)-Float64(8/9*a_n1);
+recursion32(a_n0::Float32, a_n1::Float32) = Float32(2*a_n0-8/9*a_n1);
+recursionBig(a_n0::BigFloat, a_n1::BigFloat) = BigFloat(BigFloat(2*a_n0)-BigFloat(8/9*a_n1));
+
+
+# Plot the real value
 function plot_true()
     x = 1:80
     y = 3/2*(2/3).^x
-
-    Plots.plot(x,y, label="True Function", color="red", style="dot", width=2)
+    plt[:plot](x,y, label="True Function", color="red", ls="--", linewidth=3)
 end
